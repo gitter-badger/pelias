113fe56d0df1429483600bb7969e73346ada4786
Peter Johnson <wiz@wiz.co.nz>
2014-04-25 17:02:03 +0700

    completed country code list task and cli runner

--- geonames/task/countrycodes.js
+++ geonames/task/countrycodes.js
@@ -1,6 +1,21 @@
 
-var countryInfo = require('../metadata/countryInfo');
+var Table = require('cli-table'),
+    countryInfo = require('../metadata/countryInfo');
+
+var columns = [ 'ISO', 'Country', 'Capital', 'Continent', 'Languages', 'geonameid' ];
 
 module.exports = function(){
-  return Object.keys( countryInfo );
+  
+  var table = new Table({
+    head: columns,
+    chars: { 'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' }
+  });
+
+  for( var code in countryInfo ){
+    table.push( columns.map( function( column ){
+      return countryInfo[code][column] || '';
+    }));
+  }
+
+  console.log( table.toString() );
 };
