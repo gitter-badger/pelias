e4175c0ce77b7cf3e5a111db20784972cb477498
Peter Johnson <wiz@wiz.co.nz>
2014-07-22 18:46:47 +0100

    update config

--- config/mappings/poi-noop.js
+++ config/mappings/poi-noop.js
@@ -4,6 +4,8 @@ var merge = require('merge');
 var schema = {
   'properties': {
     'name':             merge( {}, require('./partial/hash'), { 'index': 'no' } ),
+    'address':          merge( {}, require('./partial/hash'), { 'index': 'no' } ),
+    'type':             merge( {}, require('./partial/admin'), { 'store': 'no', 'index': 'no' } ),
     'admin0':           merge( {}, require('./partial/admin'), { 'index': 'no' } ),
     'admin1':           merge( {}, require('./partial/admin'), { 'index': 'no' } ),
     'admin2':           merge( {}, require('./partial/admin'), { 'index': 'no' } ),
--- config/mappings/poi.js
+++ config/mappings/poi.js
@@ -4,6 +4,8 @@ var merge = require('merge');
 var schema = {
   'properties': {
     'name':             require('./partial/hash'),
+    'address':          merge( {}, require('./partial/hash'), { 'index': 'no' } ),
+    'type':             merge( {}, require('./partial/admin'), { 'store': 'no' } ),
     'admin0':           require('./partial/admin'),
     'admin1':           require('./partial/admin'),
     'admin2':           require('./partial/admin'),
